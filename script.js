937a733e9fdf226dfabb777f0ef172387a3396cd
Arthur <106761899+asahakian@users.noreply.github.com>
2023-03-28 00:24:37 +0400

    Created New Section

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const download = document.querySelector(".download");
+const appstore = document.querySelector(".appstore");
+const googleplay = document.querySelector(".googleplay")
 
 download.addEventListener("click", (evt) => {
-    window.location.href = "https://github.com/asahakian"
+    window.location.href = "https://play.google.com/store/games?pli=1"
+});
+
+appstore.addEventListener("click", (evt) => {
+    window.location.href = "https://www.apple.com/app-store/"
+});
+
+googleplay.addEventListener("click", (evt) => {
+    window.location.href = "https://play.google.com/store/games?pli=1"
 });
 
 const secondSectionFirstBlock = document.querySelector(".secondSectionFirstBlock");
@@ -17,6 +27,9 @@ const numberThree = document.querySelector(".numberThree");
 
 const usersResponsive = document.querySelector(".usersResponsive");
 
+const fivethSectionBlock = document.querySelector(".fivethSectionBlock");
+const fivethSectionBlockResponsive = document.querySelector(".fivethSectionBlockResponsive");
+
 let sec = 0;
 let sec2 = 0;
 let sec3 = 0;
@@ -26,43 +39,45 @@ numberOne.innerHTML = sec;
 numberTwo.innerHTML = sec2;
 numberThree.innerHTML = sec;
 
-let scroll800 = true;
-let scroll1400 = true;
-let scroll2200 = true;
-let scroll2650 = true;
-let scroll2900 = true;
+let scroll470 = true;
+let scroll1160 = true;
+let scroll1900 = true;
+let scroll2630 = true;
+let scroll2750 = true;
 let scroll3032 = true;
-let scroll4300 = true;
+let scroll3450 = true;
+let scroll4130 = true;
+let scroll5050 = true;
 
 document.addEventListener("scroll", (evt) => {
     let scroll = Math.floor(scrollY);
 
-    if (scroll800 && scroll >= 800) {
+    if (scroll470 && scroll >= 470) {
         secondSectionFirstBlock.style.opacity = "1";
-        scroll800 = false;
+        scroll470 = false;
     }
 
-    if (scroll1400 && scroll >= 1400) {
+    if (scroll1160 && scroll >= 1160) {
         secondSectionSecondBlock.style.opacity = "1";
-        scroll1400 = false;
+        scroll1160 = false;
     }
 
-    if (scroll2200 && scroll >= 2200) {
+    if (scroll1900 && scroll >= 1900) {
         secondSectionThirdBlock.style.opacity = "1";
-        scroll2200 = false;
+        scroll1900 = false;
     }
 
-    if (scroll2650 && scroll >= 2650) {
+    if (scroll2630 && scroll >= 2630) {
         partOneTxt.style.opacity = "1";
-        scroll2650 = false;
+        scroll2630 = false;
     }
 
-    if (scroll2900 && scroll >= 2900) {
+    if (scroll2750 && scroll >= 2750) {
         statues.forEach((item) => {
             item.style.opacity = "1";
         })
 
-        scroll2900 = false;
+        scroll2750 = false;
     }
 
     if (scroll3032 && scroll >= 3032) {
@@ -102,8 +117,18 @@ document.addEventListener("scroll", (evt) => {
         scroll3032 = false;
     }
 
-    if (scroll4300 && scroll >= 4300) {
+    if (scroll4130 && scroll >= 4130) {
         usersResponsive.style.opacity = "1";
-        scroll4300 = false;
+        scroll4130 = false;
+    }
+
+    if(scroll3450 && scroll >= 3450) {
+        fivethSectionBlock.style.opacity = "1";
+        scroll3450 = false;
+    }
+
+    if(scroll5050 && scroll >= 5050) {
+        fivethSectionBlockResponsive.style.opacity = "1";
+        scroll5050 = false;
     }
 })
